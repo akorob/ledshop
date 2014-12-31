5e5fcb87a9ff8d526b6a632b7ef9436647b261ec
longday <6600gm@gmail.com>
2014-12-31 17:55:34 +0300

    update app libs add loading bar

--- src/main/java/com/spring/controllers/DefaultController.java
+++ src/main/java/com/spring/controllers/DefaultController.java
@@ -11,11 +11,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class DefaultController extends AbstractController {
     @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
-                                                 HttpServletResponse arg1) throws Exception {
-        // TODO Auto-generated method stub
-        System.out.println("haaaaaaaaaaaaaaaaaaaaaaaaaaaaandleRequestInternal");
-
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("/resources/index.html");
         return mv;
--- src/main/webapp/WEB-INF/resources/init/config.js
+++ src/main/webapp/WEB-INF/resources/init/config.js
@@ -3,34 +3,40 @@
  */
 'use strict'
 
-app.config([ '$routeProvider', '$httpProvider', 'localStorageServiceProvider', '$locationProvider',
-    function($routeProvider, $httpProvider, localStorageServiceProvider, $locationProvider) {
+app.config([
+    '$routeProvider', '$httpProvider', 'localStorageServiceProvider', '$locationProvider', 'cfpLoadingBarProvider',
+    function($routeProvider, $httpProvider, localStorageServiceProvider, $locationProvider, cfpLoadingBarProvider) {
+
+    // ======= Loading Bar configuration ========
+
+        cfpLoadingBarProvider.latencyThreshold = 700;
 
     // ======= local storage configuration ========
 
-    localStorageServiceProvider.prefix = 'example';
+        localStorageServiceProvider.prefix = 'example';
 
     // ======= router configuration =============
-    $locationProvider.html5Mode(true);
-    $routeProvider
-        .when('/', {
-            controller:'MainController',
-            templateUrl: 'resources/partials/main.html'
-        })
-        .when('/details', {
-            controller: 'DetailsController',
-            templateUrl: 'resources/partials/product-details.html'
-        })
-        .when('/login', {
-            controller: 'LoginController',
-            templateUrl: 'resources/partials/login.html'
-        })
-        .when('/cart', {
-            controller: 'CartController',
-            templateUrl: 'resources/partials/cart.html'
-        })
-        .when('/404', {
-            templateUrl: 'resources/partials/404.html'
-        });
-      //  .otherwise({ redirectTo : "/404"});
+
+        $locationProvider.html5Mode(true);
+        $routeProvider
+            .when('/', {
+                controller:'MainController',
+                templateUrl: 'resources/partials/main.html'
+            })
+            .when('/details', {
+                controller: 'DetailsController',
+                templateUrl: 'resources/partials/product-details.html'
+            })
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: 'resources/partials/login.html'
+            })
+            .when('/cart', {
+                controller: 'CartController',
+                templateUrl: 'resources/partials/cart.html'
+            })
+            .when('/404', {
+                templateUrl: 'resources/partials/404.html'
+            })
+            .otherwise({ redirectTo : "/404"});
 }]);
